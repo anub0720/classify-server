f10e59981fd141b80cd8e7ff099c76f33e74ca35
Anubhab Bhattacharya <anubhabbhattacharya0710@gmail.com>
2024-08-04 10:00:05 +0530

    Added gitignore

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const cors=require("cors");
 if(process.env.NODE_ENV!="production"){
   require("dotenv").config({path:".env"});
 }
@@ -10,6 +11,14 @@ const methodOverride = require("method-override");
 const MongoStore=require("connect-mongo");
 app.use(flash());
 const DB=process.env.DB;
+const corsOptions = {
+  origin: "http://localhost:5173",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 app.use(methodOverride("_method"));
 main().then(() => { console.log("connection succesful"); })
   .catch((err) => { console.log(err); });
